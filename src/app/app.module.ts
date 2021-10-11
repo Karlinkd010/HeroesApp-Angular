e0c112fbb16199cdfeaf5dc9678efceb58d55a50
DESKTOP-9JG8FU1\Karlin <karlinkd010@gmail.com>
2021-10-11 15:59:48 -0500

    Aadd components/modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
