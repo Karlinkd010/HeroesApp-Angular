3e9e09d93af66c8b4cb589538b0e13968633bf25
KarlinCruz <88855520+KarlinCruz@users.noreply.github.com>
2021-12-07 16:36:05 -0600

    Listado-Tatjeta

--- src/app/heroes/components/heroes-tarjeta/heroes-tarjeta.component.ts
+++ src/app/heroes/components/heroes-tarjeta/heroes-tarjeta.component.ts
@@ -4,8 +4,11 @@ import { Heroes } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroes-tarjeta',
   templateUrl: './heroes-tarjeta.component.html',
-  styles: [
-  ]
+  styles: [`
+  mat-card{
+    margin-top:20px;
+  }
+  `]
 })
 export class HeroesTarjetaComponent{
 @Input() heroe!:Heroes;
