43f1845c288aa7635bb382da91d6bb8212f8215a
karlinkd010 <karlinkd010@gmail.com>
2022-02-14 13:53:42 -0600

    Form desing Component Heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,6 +1,6 @@
 import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from "rxjs/operators";
 import { Heroes } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
@@ -8,15 +8,20 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    img{
+      width:100%;
+      border-radius:5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
   heroe!:Heroes;
 
   constructor(
     private activateRoute: ActivatedRoute,
-    private serviceHeroe : HeroesService
+    private serviceHeroe : HeroesService,
+    private route: Router
   ) { }
 
   ngOnInit(): void {
@@ -27,6 +32,9 @@ export class HeroeComponent implements OnInit {
         )
       )
       .subscribe(heroe=> this.heroe=heroe)
+  }
+  regresar(){
+    this.route.navigate(['/heroes/listado'])
   }
 
 }
