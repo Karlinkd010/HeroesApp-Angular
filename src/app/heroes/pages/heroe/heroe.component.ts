4795113b27b1efd4c09da37ab2452ffb5fb45b28
karlinkd010 <karlinkd010@gmail.com>
2022-02-14 13:03:10 -0600

    add component heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,9 @@
+import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from "rxjs/operators";
+import { Heroes } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HeroeComponent implements OnInit {
+  heroe!:Heroes;
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private serviceHeroe : HeroesService
+  ) { }
 
   ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(
+          ({id})=> this.serviceHeroe.getHeroePorId(id)
+        )
+      )
+      .subscribe(heroe=> this.heroe=heroe)
   }
 
 }
--- src/app/material-desing/material-desing.module.ts
+++ src/app/material-desing/material-desing.module.ts
@@ -3,16 +3,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
+import { MatListModule} from '@angular/material/list';
+import { MatCardModule} from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule} from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
+import { MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatGridListModule} from '@angular/material/grid-list';
+
 @NgModule({
   declarations: [],
   exports: [
     MatSidenavModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
