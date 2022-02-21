4dcf487079d65d9e58fac690d72697f5fb65fe7b
karlinkd010 <karlinkd010@gmail.com>
2022-02-21 17:09:12 -0600

    logout component

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,26 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent{
 
+  auth!:AuthService;
+
   constructor(
-    private router:Router
+    private router:Router,
+    private authService:AuthService
   ) { }
 
   login(){
+    this.authService.login()
+      .subscribe(resp=>{
+        // this.auth=res;
+        console.log(resp);
 
-    this.router.navigate(['./heroes']);
-
+        if(resp!=null){
+          this.router.navigate(['./heroes']);
+        }
+        
+      })
+    // 
   }
+  
 
 }
