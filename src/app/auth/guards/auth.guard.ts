dc295eb6a6b363f1b363e95fb71f402c25e88554
karlinkd010 <karlinkd010@gmail.com>
2022-02-22 09:58:03 -0600

    Create auth.guard.ts

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements  CanLoad {
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+      console.log('Can', true);
+      console.log(route);
+      console.log(segments);
+    return true;
+  }
+}
