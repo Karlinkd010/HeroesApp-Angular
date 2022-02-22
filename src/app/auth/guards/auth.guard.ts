9b4a2a3b6e10f1f0449a040a28313c8a78ab7eee
karlinkd010 <karlinkd010@gmail.com>
2022-02-22 10:45:38 -0600

    Update auth.guard.ts

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  CanLoad {
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+export class AuthGuard implements  CanLoad , CanActivate{
+
+  constructor(
+    private authService: AuthService
+  ){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-      console.log('Can', true);
-      console.log(route);
-      console.log(segments);
-    return true;
+
+      if(this.authService.auth.id){
+        console.log('Homepage');
+        console.log(route,segments)
+        return true;
+      }
+      console.log('locked');
+    return false;
   }
 }
