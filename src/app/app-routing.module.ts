8c8f124be707e828ea80dcd4bcef3587aef90631
karlinkd010 <karlinkd010@gmail.com>
2022-02-22 09:58:07 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import {  RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes:Routes=[
@@ -9,7 +10,8 @@ const routes:Routes=[
   },
   {
     path:'heroes',
-    loadChildren: () =>import('./heroes/heroes.module').then(m => m.HeroesModule)
+    loadChildren: () =>import('./heroes/heroes.module').then(m => m.HeroesModule),
+    canLoad:[AuthGuard]
   },
   {
     path:'404',
